a142654ee362e1ea673f94f461b42f1d808b2db6
Samim Shammo <you@example.comsamimshammo>
2021-08-21 16:15:41 +0600

    first commit

---
+++ jstring.js
@@ -0,0 +1,66 @@
+// const names = 'sakiB';
+// const namest ='sakib' ;
+// if(names.toLowerCase() == namest.toUpperCase()){
+//    console.log('o')
+// }
+// else{
+//    console.log('no')
+//  }
+// const totalname = names.concat(namest).concat(3);
+// console.log(totalname)
+
+
+//  const anthem = 'amr shonar bangla ami tomay valobahsi';
+// //  const word = anthem.split('');
+//  const nes = anthem.splice(5,10)
+// const arry = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//  console.log(word);
+//  console.log(nes)
+// const ss = anthem.substring(10, 22)
+// console.log(ss)
+// const newname = arry.splice(4, 5, 42, 23, 43, 21, 24 )
+// console.log(newname)
+// console.log(arry);
+
+// const numbers = [ 2, 3, 6, 5, 1, 7, 9, 4]
+// const sortedName = numbers.sort();
+// console.log(sortedName);
+
+// const shortName = ['sabbir', 'kabbir', 'labbib', 'bakid', 'yealo', 'kalu', 'rabbi']
+// const shortName = [4, 5, 42, 23, 43, 21, 24 ]
+// const shortnames = shortName.sort( function(a, b){
+//    return a-b;
+// });
+// // const shortnanewmes = shortnames.reverse();
+
+// console.log(shortnames);
+
+// function getPlus(num1, num2){
+//    let = result = 0; 
+//    for(const num of arguments){
+//       result = result + num;
+//    }
+//    return result;
+// }
+// const number = getPlus(20, 30, 23)
+// console.log(number);
+
+// function getName( name1, nam2){
+//    let nams = '';
+//    for(const part of arguments){
+//       nams = nams + part + ' ';
+//    }
+//    return nams
+// }
+// let nwes = getName('samim', 'shammo')
+// console.log(nwes)
+
+// const task = "Practice Makes a person perfect";
+
+// const index = task.toLowerCase().indexOf("M");
+// console.log(index)
+// const str1 = 'Bangladesh is a beautiful country.'; console.log(str1.endsWith("country"));
+// const str= ['I', 'love', 'coding', 'more', 'than', 'eating.'];
+// const newsa = str.reverse()
+// console.log(newsa)
+
